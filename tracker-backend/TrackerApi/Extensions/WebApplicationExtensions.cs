c03fd2cbb71efa58c8f738de98f38edb6071c665
thiagocarvalho93 <thiagopaes1993@gmail.com>
2026-02-02 11:09:09 -0300

    ♻️ [BACK] Web app extension methods on single file

--- tracker-backend/TrackerApi/Extensions/WebApplicationExtensions.cs
+++ tracker-backend/TrackerApi/Extensions/WebApplicationExtensions.cs
@@ -1,10 +1,46 @@
 using Microsoft.AspNetCore.Builder;
+using TrackerApi.Configuration;
 using TrackerApi.Middlewares;
+using TrackerApi.Repositories;
+using TrackerApi.Repositories.Interfaces;
+using TrackerApi.Services;
+using TrackerApi.Services.Interfaces;
 
 namespace TrackerApi.Extensions;
 
 public static class WebApplicationExtensions
 {
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddHttpsRedirection(options =>
+        {
+            options.HttpsPort = 443;
+        });
+
+        // DI
+        builder.Services.AddScoped<ITrackerService, TrackerService>();
+        builder.Services.AddScoped<IPathRepository, PathRepository>();
+        builder.Services.AddSingleton<IStateService, StateService>();
+
+        builder.Services.Configure<PathSettings>(
+            builder.Configuration.GetSection("PathSettings"));
+
+        // CORS
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins", policy =>
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
+        });
+
+        return builder;
+    }
+
     public static WebApplication RegisterMiddlewares(this WebApplication app)
     {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
